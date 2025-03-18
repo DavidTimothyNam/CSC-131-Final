e931c79f3ab37d72ce82741409b2ca85d17e0f5f
Ahmad Awadalla <167709857 + ahmadawadalla@users.noreply.github.com>
2025-03-18 12:12:55 -0700

    added contact page

---
+++ Website/contact.js
@@ -0,0 +1,35 @@
+function commentCharacterLimit(){
+    $('textarea').keyup(function() {
+
+        var characterCount = $('.message-box textarea').val().length,
+            current = $('#current');
+
+        current.text(characterCount);
+
+
+        /*This isn't entirely necessary, just playin around*/
+        if (characterCount < 100) {
+            current.css('font-weight', '400')
+        }
+        else if (characterCount > 99 && characterCount < 200) {
+            current.css('color', '#6d5555');
+            current.css('font-weight', '450')
+        }
+        else if (characterCount > 199 && characterCount < 300) {
+            current.css('color', '#793535');
+            current.css('font-weight', '500')
+        }
+        else if (characterCount > 299 && characterCount < 400) {
+            current.css('color', '#841c1c');
+            current.css('font-weight', '550');
+        }
+        else if (characterCount > 399 && characterCount < 500){
+            current.css('color', '#6e0003');
+            current.css('font-weight', '600');
+        }
+        else {
+            current.css('color', '#5a0103');
+            current.css('font-weight', '650');
+        }
+    });
+}
