cd2989d9cec81394100e737cb55f1f14480342d9
Ahmad Awadalla <167709857 + ahmadawadalla@users.noreply.github.com>
2025-03-19 00:38:23 -0700

    editted contact page

--- Website/contact.js
+++ Website/contact.js
@@ -1,7 +1,7 @@
-function commentCharacterLimit(){
+function commentCharacterLimit(id){
     $('textarea').keyup(function() {
 
-        var characterCount = $('#floatingTextarea2Disabled').val().length,
+        var characterCount = $(`#${id}`).val().length,
             current = $('#current');
 
         current.text(characterCount);
@@ -9,15 +9,16 @@ function commentCharacterLimit(){
 
         /*This isn't entirely necessary, just playin around*/
         if (characterCount < 100) {
-            current.css('font-weight', '400')
+            current.css('color','black');
+            current.css('font-weight', '400');
         }
         else if (characterCount > 99 && characterCount < 200) {
             current.css('color', '#6d5555');
-            current.css('font-weight', '450')
+            current.css('font-weight', '450');
         }
         else if (characterCount > 199 && characterCount < 300) {
             current.css('color', '#793535');
-            current.css('font-weight', '500')
+            current.css('font-weight', '500');
         }
         else if (characterCount > 299 && characterCount < 400) {
             current.css('color', '#841c1c');
@@ -32,4 +33,79 @@ function commentCharacterLimit(){
             current.css('font-weight', '650');
         }
     });
+}
+
+function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function checkIfValid(id){
+    let input = document.getElementById(id)
+    if(id === 'floatingInputName'){
+        if(!input.value){ // if invalid
+            input.className += ' is-invalid'
+        }
+        else { // valid
+            input.className = 'form-control'
+        }
+    }
+    else if(id === 'floatingInputEmail'){
+        if(!input.value || !isValidEmail(input.value)){ // if invalid
+            input.className += ' is-invalid'
+        }
+        else { // valid
+            input.className = 'form-control'
+        }
+    }
+    else if(id === 'floatingSelectTopic'){
+        if(input.value === 'Select An Option'){ // if invalid
+            input.className += ' is-invalid'
+        }
+        else { // valid
+            input.className = 'form-control'
+        }
+    }
+    else if(id === 'floatingInputComment'){
+        if(!input.value){ // if invalid
+            input.className += ' is-invalid'
+        }
+        else { // valid
+            input.className = 'form-control'
+        }
+    }
+}
+
+function checkBoxes(){
+    let input = document.getElementById('floatingInputName')
+    if(!input.value){ // if invalid
+        input.className += ' is-invalid'
+    }
+    else { // valid
+        input.className = 'form-control'
+    }
+
+    input = document.getElementById('floatingInputEmail')
+    if(!input.value || !isValidEmail(input.value)){ // if invalid
+        input.className += ' is-invalid'
+    }
+    else { // valid
+        input.className = 'form-control'
+    }
+
+    input = document.getElementById('floatingSelectTopic')
+    if(input.value === 'Select An Option'){ // if invalid
+        input.className += ' is-invalid'
+    }
+    else { // valid
+        input.className = 'form-control'
+    }
+
+    input = document.getElementById('floatingInputComment')
+    if(!input.value){ // if invalid
+        input.className += ' is-invalid'
+    }
+    else { // valid
+        input.className = 'form-control'
+    }
 }
