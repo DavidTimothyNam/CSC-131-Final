2d67cceceefcef5ab1b4bbfd3b02aed7b3836ba3
DavidTimothyNam <87089167+DavidTimothyNam@users.noreply.github.com>
2025-03-24 21:32:59 -0700

    created blank About, Home, Contact with navbar

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PageNavbar from "../components/Navbar";
+
+const Contact = () => {
+  return (
+    <>
+      <PageNavbar />
+    </>
+  );
+};
+
+export default Contact;
