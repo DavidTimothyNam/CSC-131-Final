caaa0f8f0ba0341125f3d58fb1405f8a8baf8433
DavidTimothyNam <87089167+DavidTimothyNam@users.noreply.github.com>
2025-04-15 19:32:32 -0700

    complete about us page

--- src/pages/AboutUs/Testimonials.jsx
+++ src/pages/AboutUs/Testimonials.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import PageNavbar from "../../components/Navbar";
-import TestimonialCard from "../../components/Testimonials/TestimonialCard";
+// import TestimonialCard from "../../components/Testimonials/TestimonialCard";
+import TestimonialsSection from "../../components/Testimonials/TestimonialsSection";
+import Footer from "../../components/Footer/Footer";
+import { Container } from "react-bootstrap";
 
 const Testimonials = () => {
   return (
     <>
       <PageNavbar />
-      <h1>Testimonials</h1>
-      <TestimonialCard
+      <Container className="my-5">
+        <h1 className="text-center mb-4">Testimonials</h1>
+        {/* <h1>Testimonials</h1> */}
+        {/* <TestimonialCard
         image="/images/clientPlaceholders/client1.jpg"
         testimony="Working with Ron Smithey's Financial Services has been a game-changer for my business. Their expert advice helped me reduce expenses and increase my revenue by 20% in just six months!"
         name="Sarah W."
@@ -21,7 +26,10 @@ const Testimonials = () => {
         image="/images/clientPlaceholders/client3.jpg"
         testimony="I was overwhelmed with managing my retirement funds, but Ron Smithey and his team provided a clear plan that gave me confidence and peace of mind. Now, I know my future is secure."
         name="Jessica M."
-      />
+      /> */}
+        <TestimonialsSection />
+      </Container>
+      <Footer />
     </>
   );
 };
