b4667f34a35ab1c50b95aef6dd7d4cc331bc3c2a
DavidTimothyNam <87089167+DavidTimothyNam@users.noreply.github.com>
2025-04-16 13:11:25 -0700

    update and use Layout.jsx on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,28 +6,29 @@ import ResourceSection from "../components/ResourceSection/ResourceSection";
 import ServiceSection from "../components/ServicesSection/ServicesSection";
 import Footer from "../components/Footer/Footer";
 import BlogList from "../components/BlogSection";
+import Layout from "../components/Layout";
 
 class Home extends React.Component {
   render() {
     return (
       <>
-        <PageNavbar />
-        <Container>
-          <AboutHero />
-        </Container>
-        <Container className="mt-4">
-          <h2 className="text-center section-title">Read Our Blog</h2>
-          <BlogList />
-        </Container>
-        <Container className="mt-4">
-          <h2 className="text-center mb-4">Financial Resources</h2>
-          <ResourceSection />
-        </Container>
-        <Container className="mt-4">
-          <h2 className="text-center mb-4">What We Offer</h2>
-          <ServiceSection />
-        </Container>
-        <Footer />
+        <Layout>
+          <Container>
+            <AboutHero />
+          </Container>
+          <Container className="mt-4">
+            <h2 className="text-center section-title">Read Our Blog</h2>
+            <BlogList />
+          </Container>
+          <Container className="mt-4">
+            <h2 className="text-center mb-4">Financial Resources</h2>
+            <ResourceSection />
+          </Container>
+          <Container className="mt-4">
+            <h2 className="text-center mb-4">What We Offer</h2>
+            <ServiceSection />
+          </Container>
+        </Layout>
       </>
     );
   }
