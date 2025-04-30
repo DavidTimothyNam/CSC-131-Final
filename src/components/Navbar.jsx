2238432196d4773dde123d2abd86f7a2354a17f2
DavidTimothyNam <87089167+DavidTimothyNam@users.noreply.github.com>
2025-04-30 13:50:06 -0700

    fixed broken styles?

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ import {
   Button,
 } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import "/src/styles.css";
+// import "/src/styles.css";
 
 function PageNavbar() {
   return (
