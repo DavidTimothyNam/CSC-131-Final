cce4048f4e538b94b203c64848780bef6d29bd28
DavidTimothyNam <87089167+DavidTimothyNam@users.noreply.github.com>
2025-04-30 16:17:09 -0700

    fix calcButton styles being overwritten

--- src/components/CalculatorComponents/CalculatorSelector.jsx
+++ src/components/CalculatorComponents/CalculatorSelector.jsx
@@ -1,24 +1,62 @@
-import React, { useState } from 'react';
-import { Accordion, ButtonGroup, ToggleButton } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Accordion, ButtonGroup, ToggleButton } from "react-bootstrap";
 import CalculatorField from "../../components/CalculatorComponents/calculatorField.jsx";
-import calculatorData from '../../data/calculatorData.json';
-
+import calculatorData from "../../data/calculatorData.json";
 
 const CalculatorSelector = () => {
   const [selected, setSelected] = useState(1);
 
   const calculators = [
-    { id: 'loanCredit', title: 'Loan & Credit Cards', calcs: ['Loan Payoff', 'Home Affordability', 'Mortage Refinancing', 'Mortage Acceleration', 'Credit Card Debt', 'Lease Payment', 'Car Affordability'], keys: [1,2,3,4,5,6,7]},
-    { id: 'saveInvest', title: 'Saving & Investing', calcs: ['College Funding', 'Saving Goals', 'Savings Accumulation', 'Taxable Equivalent Yield'], keys: [8,9,10,11]},
-    { id: 'cashManage', title: 'Cash Management', calcs: ['Cash Flow Management', 'Net Worth', 'Lifetime Earnings'], keys: [12,13,14]},
-    { id: 'taxesIRA', title: 'Taxes & IRAS', calcs: ['Federal Incom Tax', ' Tax-Deferred Savings', 'IRA Eligibility', 'Roth IRA Conversion', 'Estate Taxes'], keys: [15,16,17,18,19]}
+    {
+      id: "loanCredit",
+      title: "Loan & Credit Cards",
+      calcs: [
+        "Loan Payoff",
+        "Home Affordability",
+        "Mortage Refinancing",
+        "Mortage Acceleration",
+        "Credit Card Debt",
+        "Lease Payment",
+        "Car Affordability",
+      ],
+      keys: [1, 2, 3, 4, 5, 6, 7],
+    },
+    {
+      id: "saveInvest",
+      title: "Saving & Investing",
+      calcs: [
+        "College Funding",
+        "Saving Goals",
+        "Savings Accumulation",
+        "Taxable Equivalent Yield",
+      ],
+      keys: [8, 9, 10, 11],
+    },
+    {
+      id: "cashManage",
+      title: "Cash Management",
+      calcs: ["Cash Flow Management", "Net Worth", "Lifetime Earnings"],
+      keys: [12, 13, 14],
+    },
+    {
+      id: "taxesIRA",
+      title: "Taxes & IRAS",
+      calcs: [
+        "Federal Incom Tax",
+        " Tax-Deferred Savings",
+        "IRA Eligibility",
+        "Roth IRA Conversion",
+        "Estate Taxes",
+      ],
+      keys: [15, 16, 17, 18, 19],
+    },
   ];
 
   return (
     <div className="container">
-      <div className = "row text-start mb-4">
-        <div className = "col-3">
-        <h1 className="bold text-center">Calculators</h1>
+      <div className="row text-start mb-4">
+        <div className="col-3">
+          <h1 className="bold text-center">Calculators</h1>
         </div>
         <div className="col-9"></div>
       </div>
@@ -29,23 +67,24 @@ const CalculatorSelector = () => {
               <Accordion.Item eventKey={listNum.toString()} key={calc.id}>
                 <Accordion.Header>{calc.title}</Accordion.Header>
                 <Accordion.Body>
-                <div className="d-flex justify-content-center">
-                  <ButtonGroup vertical>
-                    {calc.calcs.map((calc2, index) => (
-                      <ToggleButton
-                        key={calc.keys[index]}
-                        id={calc2}
-                        type="radio"
-                        name="btnradio"
-                        value={calc2}
-                        checked={selected === calc.keys[index]}
-                        onChange={() => setSelected(calc.keys[index])}
-                        className="btn btn-primary calcButton"
-                      >
-                        {calc2}
-                      </ToggleButton>
-                    ))}
-                  </ButtonGroup>
+                  <div className="d-flex justify-content-center">
+                    <ButtonGroup vertical>
+                      {calc.calcs.map((calc2, index) => (
+                        <ToggleButton
+                          key={calc.keys[index]}
+                          id={calc2}
+                          type="radio"
+                          name="btnradio"
+                          value={calc2}
+                          checked={selected === calc.keys[index]}
+                          onChange={() => setSelected(calc.keys[index])}
+                          className="btn btn-primary calcButton"
+                          variant="none"
+                        >
+                          {calc2}
+                        </ToggleButton>
+                      ))}
+                    </ButtonGroup>
                   </div>
                 </Accordion.Body>
               </Accordion.Item>
@@ -53,7 +92,9 @@ const CalculatorSelector = () => {
           </Accordion>
         </div>
         <div className="col-2"></div>
-        <div className="col-6"><CalculatorField data={calculatorData[selected-1]}/></div>
+        <div className="col-6">
+          <CalculatorField data={calculatorData[selected - 1]} />
+        </div>
       </div>
     </div>
   );
