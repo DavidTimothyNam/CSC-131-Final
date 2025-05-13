1328b03268ff035ec720cab33f51919207e4319c
DavidTimothyNam <87089167+DavidTimothyNam@users.noreply.github.com>
2025-05-12 18:33:35 -0700

    replace logo and favicon

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ function PageNavbar() {
       <Container>
         <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
           <img
-            src="/rs_financial_logo1.png"
+            src="/public/rs_financial_logo_light.png"
             alt="Logo"
             height="30"
             className="me-2"
